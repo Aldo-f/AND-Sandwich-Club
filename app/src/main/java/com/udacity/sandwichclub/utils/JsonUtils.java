bba14e77e1c68303fbe4d561a7af136d0883f800
Aldo-f <aldo.fieuw@gmail.com>
2018-02-28 18:39:34 +0100

    Tried out all the suggestions made by my reviewer

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,27 +11,38 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String SUB_OBJECT = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String ALSO_KNOWN_AS_ARRAY = "alsoKnownAs";
+    public static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE_PATH = "image";
+    public static final String INGREDIENTS_ARRAY = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) {
+
 
         try {
             // 1
             JSONObject rootObject = new JSONObject(json);
 
             // 1.1
-            JSONObject subObject = rootObject.getJSONObject("name");
+            JSONObject subObject = rootObject.getJSONObject(SUB_OBJECT);
 
             // 1.1.1
-            String mainName = subObject.getString("mainName");
-            JSONArray alsoKnownAsArray = subObject.getJSONArray("alsoKnownAs");
+            // https://stackoverflow.com/questions/13790726/the-difference-between-getstring-and-optstring-in-json/13790789#13790789
+            String mainName = subObject.optString(MAIN_NAME);
+            JSONArray alsoKnownAsArray = subObject.getJSONArray(ALSO_KNOWN_AS_ARRAY);
 
             // 1.1
-            String placeOfOrigin = rootObject.getString("placeOfOrigin");
+            String placeOfOrigin = rootObject.getString(PLACE_OF_ORIGIN);
             if (placeOfOrigin.isEmpty()) {
                 placeOfOrigin = "Unknown";
             }
-            String description = rootObject.getString("description");
-            String imagePath = rootObject.getString("image");
-            JSONArray ingredientsArray = rootObject.getJSONArray("ingredients");
+            String description = rootObject.optString(DESCRIPTION);
+            String imagePath = rootObject.optString(IMAGE_PATH);
+            JSONArray ingredientsArray = rootObject.getJSONArray(INGREDIENTS_ARRAY);
 
 
             //Iterate through the array of alsoKnowAsArray and add it to list
